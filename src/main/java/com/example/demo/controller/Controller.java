98fcf3d289cddccf35ba556f169cbbedbd3f4d62
Lo, Penny <Penny.Lo@lexisnexisrisk.com>
2023-01-17 09:34:54 -0800

    Added debug statement

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -28,12 +28,14 @@ public class Controller {
     }
     var num = ++lastOrder % 4;
     if (num == 0) {
+      log.debug("order: [{}], data byte length=[{}]", num, data.getBytes().length);
       return data;
     }
     var sb = new StringBuilder();
     for (int i = 0; i <= num; i++) {
       sb.append(data);
     }
+    log.debug("order: [{}], data byte length=[{}]", num, sb.toString().getBytes().length);
     return sb.toString();
   }
 
